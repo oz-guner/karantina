56e587702bc227cbd6cbec614c75a1a27333b9af
oz-guner <64649801+oz-guner@users.noreply.github.com>
2020-11-18 12:10:04 -0500

    Add files via upload

---
+++ karantina.py
@@ -0,0 +1,32 @@
+hafta_ici=['Pazartesi', 'Sali', 'Carsamba', 'Persembe', 'Cuma']
+hafta_sonu=['Cumartesi', 'Pazar']
+
+gun=input("Gun giriniz: ")
+
+if gun in hafta_ici:
+    calisiyor=input("Calisiyor musunuz? (evet/hayir): ")
+    if calisiyor=='evet':
+        print("Calisabilirsiniz.")
+    else:
+        yasiniz = input("Yasinizi giriniz: ")
+        if 20<=int(yasiniz)<65:
+            print("Cikabilirsiniz.")
+        else:
+            saat = input("Saat giriniz: ")
+            if 10<=round(float(saat))<16:
+                print("Cikabilirsiniz.")
+            else:
+                print("Cikamazsiniz.")
+
+elif gun in hafta_sonu:
+    saat = input ("Saat giriniz: ")
+    if 10<round(float(saat))<20:
+        yasiniz = input("Yasinizi giriniz: ")
+        if 20<int(yasiniz)<65:
+            print("Cikabilirsiniz.")
+        else:
+            print("Cikamazsiniz.")
+    else:
+        print("Ciamazsiniz.")
+else:
+    print("Yanlis deger. Tekrar deneyin.")
